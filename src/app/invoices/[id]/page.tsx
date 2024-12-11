ae39dff5fcb52afccfe937bb0cfd843f20811560
soliloquy-ray <santos.ray.rommel@gmail.com>
2024-12-11 01:21:28 -0500

    finish up commercial invoice

--- src/app/invoices/[id]/page.tsx
+++ src/app/invoices/[id]/page.tsx
@@ -1,10 +1,11 @@
 // pages/invoice-view/[id].tsx
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';  // Note: using navigation instead of router
 import styled from 'styled-components';
-import { Button } from 'antd';
+import { Button, Input, Row, Select, Image } from 'antd';
 import { PrinterOutlined } from '@ant-design/icons';
 import { fetchInvoices } from '@/lib/quickbooks-api';
 import dayjs from 'dayjs';
@@ -13,7 +14,15 @@ const PrintableContainer = styled.div`
   padding: 20px;
   max-width: 1200px;
   margin: 0 auto;
+  background-color: #fff;
+  color: #333;
   
+  .ant-image {
+    display: block;
+    margin: 25px auto;
+    object-fit: contain;
+  }
+
   @media print {
     padding: 0;
     .no-print {
@@ -21,6 +30,10 @@ const PrintableContainer = styled.div`
     }
   }
 `;
+
+const ExtendedRow = styled(Row)`
+  font-size: 10px;
+`
 
 const InvoiceTable = styled.table`
   width: 100%;
@@ -31,6 +44,7 @@ const InvoiceTable = styled.table`
     border: 1px solid #000;
     padding: 8px;
     text-align: left;
+    font-size: 10px;
   }
   
   th {
@@ -56,11 +70,41 @@ const PrintButton = styled(Button)`
     display: none;
   }
 `;
+const {Option} = Select;
+
+const ExtendedSelect = styled(Select)`
+height: 12px;
+.ant-select-selector {
+padding: 0 !important;
+}
+.ant-select-selection-item{
+  font-size: 10px;
+padding: 0;
+}
+`;
+
+const ExtendedInput = styled(Input)`
+font-size: 10px;
+padding: 0;
+height: 12px;
+`
+
+interface ExtendedLineItem extends LineItem {
+  hidden: boolean;
+}
 
 export default function InvoiceView() {
   const params = useParams();
   const id = params.id as string;
   const [invoice, setInvoice] = useState<Invoice | null>(null);
+  const [invoiceLine, setInvoiceLine] = useState<ExtendedLineItem[]>([]);
+  const [fourValue, setFourValue] = useState("");
+  const [fiveValue, setFiveValue] = useState("");
+  const [sixValue, setSixValue] = useState("");
+  const [nineValue, setNineValue] = useState("");
+  const [tenValue, setTenValue] = useState("");
+  const [elevenValue, setElevenValue] = useState("");
+  const [twelveValue, setTwelveValue] = useState("");
 
   useEffect(() => {
     if (id) {
@@ -80,6 +124,78 @@ export default function InvoiceView() {
     window.print();
   };
 
+  const getProductCodeFromDesc = (desc: string): string => {
+    let type = "";
+    let color = desc.includes("green") ? "Green" : "Orange";
+    if (desc.includes("master box")) {
+      type = "YT300";
+    } else if (desc.includes("box")) {
+      type = "YT30";
+    } else {
+      type = "YT1"
+    }
+    return `${type} - ${color}`;
+  }
+
+  const getPackagingFromDesc = (desc: string): JSX.Element => {
+    if (desc.includes("master box")) {
+      if (desc.includes("orange")) return <p>Approx. Dimensions of package L x W x H<br/>
+Inches: 13-5/8 x  9-1/8 x 20-1/2 <br/>
+Centimeters: 34 x 23.5 x 52</p>;
+      else return <p>Approx. Dimensions of package L x W x H<br/>
+Inches: 13-5/8 x  9-1/8 x 20-1/2 <br/>
+Centimeters: 34 x 23.5 x 52</p>;
+    }else if (desc.includes("box")) {
+      if (desc.includes("orange")) return <p>Approx. Dimensions of package L x W x H<br/>
+Inches: 8.86 x 6.69 x 3.94<br/>
+Centimeters: 22.5 x 17 x 10</p>;
+      else return <p>Approx. Dimensions of package L x W x H<br/>
+Inches: 8.86 x 6.69 x 3.94<br/>
+Centimeters: 22.5 x 17 x 10</p>;
+    } else {
+      if (desc.includes("orange")) return <p>Approx. Dimensions of package L x W x H<br/>
+Inches: 5.2 x 0.5 x 7.5 <br/>
+Centimeters: 11.5 x 1.5 x 17</p>;
+      else return <p>Approx. Dimensions of package L x W x H<br/>
+Inches: 5.2 x 0.5 x 7.5 <br/>
+Centimeters: 11.5 x 1.5 x 17</p>;
+    } 
+  }
+
+  const getWeightFromLine = (item: string, actualPkg: number, actualQty: number ): number => {
+    if (item.includes("master box")) {
+      return 5.50 * Number(actualPkg);
+    } else if (item.includes("box")) {
+      return 0.50 * Number(actualPkg);
+    } else return 0.05 * Number(actualQty);
+  }
+
+  const getTotalWeight = (lineItems: ExtendedLineItem[]): number => {
+    let gs = 0;
+    lineItems
+            ?.filter(item => ['SalesItemLineDetail','GroupLineDetail','DiscountLineDetail'].includes(item.DetailType) && item?.SalesItemLineDetail?.ItemRef.value !== "SHIPPING_ITEM_ID" && !item.SalesItemLineDetail?.ItemRef?.name?.includes('Stripe') && !item.GroupLineDetail?.GroupItemRef?.name.includes('Stripe') && !item?.hidden)
+            ?.map((item, index) => {
+              console.log({ item });
+              const actualPkg = item.GroupLineDetail?.Quantity ?? item.SalesItemLineDetail?.Qty;
+              
+            const actualQty = item.GroupLineDetail?.Line[0]?.SalesItemLineDetail?.Qty ?? item.SalesItemLineDetail?.Qty;
+              
+              const itemDescription = item.DetailType === 'GroupLineDetail' ? item.GroupLineDetail?.GroupItemRef?.name :
+              item.DetailType === 'DiscountLineDetail' ? item?.DiscountLineDetail?.DiscountAccountRef?.name : 
+            item.SalesItemLineDetail?.ItemRef.name;
+              const actualWeight = getWeightFromLine(itemDescription?.toLocaleLowerCase() ?? "", actualPkg ?? 0, actualQty ?? 0);
+              console.log({ actualPkg, actualQty, actualWeight });
+              gs += actualWeight;
+            })
+    return gs;
+  }
+
+  useEffect(() => {
+    if (invoice) {
+      setInvoiceLine(invoice.Line.map((lineItems) => ({...lineItems, hidden: false })));
+    }
+  }, [invoice])
+
   if (!invoice) return null;
 
   return (
@@ -92,8 +208,8 @@ export default function InvoiceView() {
       >
         Print Invoice
       </PrintButton>
-
-      <Title>COMMERCIAL INVOICE</Title>
+      <Image height={50} style={{objectFit: "contain"}} src={"/Yewtwist-logo.png"}/>
+      <Title style={{fontSize: 20, letterSpacing: 10}}>COMMERCIAL INVOICE</Title>
 
       <InvoiceTable>
         <tbody>
@@ -111,19 +227,20 @@ export default function InvoiceView() {
       </InvoiceTable>
 
       <InvoiceTable>
-        <tbody>
-          <tr>
-            <th colSpan={2}>3. Shipper / Exporter Information:</th>
-            <th colSpan={2}>4. Consignee / Importer Information:</th>
+        <tbody style={{display: "flex", flexDirection: "column"}}>
+          <tr style={{display: "flex"}}>
+            <th colSpan={2} style={{flex: 1}}>3. Shipper / Exporter Information:</th>
+            <th colSpan={2} style={{flex: 1}}>4. Consignee / Importer Information:</th>
           </tr>
-          <tr>
-            <td colSpan={2}>
+          <tr style={{display: "flex"}}>
+            <td style={{flex: 1}} colSpan={2}>
               Ingenyewity Inc<br />
               15 Allstate Parkway, Suite 600<br />
               L3R 5B4 Markham, Ontario<br />
               Canada
             </td>
-            <td colSpan={2}>
+            <td style={{flex: 1}} colSpan={2}>
+              <ExtendedInput value={fourValue} onChange={(v) => setFourValue(v.target.value)} style={{fontSize: 10, padding: 0, border: fourValue === "" ? "1px solid red" : ""}} bordered={false} />
               {Object.values(invoice?.BillAddr ?? {}).filter((bData) => bData !== "").map((billData, index) => <p key={index}>{billData}</p>)}
               {invoice?.CustomField?.find((cf) => cf.Name === "Contact Number")?.StringValue} 
              {/*  {invoice?.CustomerRef?.name}<br />
@@ -147,9 +264,12 @@ export default function InvoiceView() {
             <th colSpan={2}>9. Importer - If different from Consignee: (Complete name & address)</th>
           </tr>
           <tr>
-            <td>Canada</td>
+            <td><ExtendedSelect bordered={false} style={{width: "100%", border: fiveValue === "" ? "1px solid red" : ""}} value={fiveValue} onChange={(v: any)=>setFiveValue(v)}>
+              <Option value={1}>Canada</Option><Option value={2}>Switzerland</Option></ExtendedSelect>
+            </td>
             <td>Canada</td>
             <td colSpan={2} rowSpan={3}>
+              <ExtendedInput value={nineValue} onChange={(v: any) => setNineValue(v.target.value)} style={{fontSize: 10, padding: 0,border: nineValue === "" ? "1px solid red" : ""}} bordered={false} />
               {Object.values(invoice?.ShipAddr ?? {}).filter((sData) => sData !== "").map((shipData, index) => <p key={index}>{shipData}</p>)}
               {invoice?.CustomField?.find((cf) => cf.Name === "Contact Number")?.StringValue}
             {/* {invoice?.CustomerRef?.name}<br />
@@ -167,21 +287,36 @@ export default function InvoiceView() {
             <th>8. Tariff Classification:</th>
           </tr>
           <tr>
-            <td>{invoice?.ShipAddr?.Line4}</td>
+            <td>
+              <ExtendedSelect value={sixValue} onChange={(v: any) => setSixValue(v)} bordered={false} style={{width: "100%",border: sixValue === "" ? "1px solid red" : ""}}>
+                {Object.entries(invoice?.BillAddr ?? {}).map(([a, b], index) => (<Option key={index}>{b}</Option>))}                
+              </ExtendedSelect>
+              </td>
             <td>9018.9080</td>
           </tr>
         </tbody>
       </InvoiceTable>
 
       <InvoiceTable>
-        <tbody>
-          <tr >
-            <th style={{width: "50%"}}>10. International Air Waybill Number / Tracking Number:</th>
-            <th style={{width: "50%"}}>11. Incoterms:</th>
+        <tbody style={{display: "flex", flexDirection: "column"}}>
+          <tr style={{display: "flex", width: "100%"}}>
+            <th colSpan={2} style={{flex: 2}}>10. International Air Waybill Number / Tracking Number:</th>
+            <th style={{flex: 1}}>11. Incoterms:</th>
+            <th style={{flex: 1}}>12. Others</th>
           </tr>
-          <tr>
-            <td>{invoice.ShipMethodRef?.value}</td>
-            <td>F.O.B</td>
+          <tr style={{display: "flex"}}>
+            <td colSpan={2} style={{flex: 2}}><ExtendedInput value={tenValue} onChange={(v: any) => setTenValue(v.target.value)} bordered={false} style={{width: "100%", border: tenValue === "" ? "1px solid red" : ""}}/></td>
+            <td style={{flex: 1}}><ExtendedSelect value={elevenValue} onChange={(v: any) => setElevenValue(v)} bordered={false} style={{width: "100%", border: elevenValue === "" ? "1px solid red" : ""}}>
+              <Option>F.O.B.</Option>
+              <Option>E.X.W.</Option>
+              </ExtendedSelect>
+            </td>
+            <td style={{flex: 1}}>
+              <ExtendedSelect bordered={false} value={twelveValue} onChange={(v: any) => setTwelveValue(v)} style={{width: "100%", border: twelveValue === "" ? "1px solid red" : ""}} allowClear>
+                <Option value={1}>Product Sample Not For Sale</Option>
+                <Option value={2}>{" "}</Option>
+              </ExtendedSelect>
+            </td>
           </tr>
         </tbody>
       </InvoiceTable>
@@ -202,8 +337,8 @@ export default function InvoiceView() {
           </tr>
         </thead>
         <tbody>
-          {invoice?.Line
-            ?.filter(item => ['SalesItemLineDetail','GroupLineDetail','DiscountLineDetail'].includes(item.DetailType) && item?.SalesItemLineDetail?.ItemRef.value !== "SHIPPING_ITEM_ID" && !item.SalesItemLineDetail?.ItemRef?.name?.includes('Stripe') && !item.GroupLineDetail?.GroupItemRef?.name.includes('Stripe'))
+          {invoiceLine
+            ?.filter(item => ['SalesItemLineDetail','GroupLineDetail','DiscountLineDetail'].includes(item.DetailType) && item?.SalesItemLineDetail?.ItemRef.value !== "SHIPPING_ITEM_ID" && !item.SalesItemLineDetail?.ItemRef?.name?.includes('Stripe') && !item.GroupLineDetail?.GroupItemRef?.name.includes('Stripe') && !item?.hidden)
             ?.map((item, index) => {
               const itemDescription = item.DetailType === 'GroupLineDetail' ? item.GroupLineDetail?.GroupItemRef?.name :
               item.DetailType === 'DiscountLineDetail' ? item?.DiscountLineDetail?.DiscountAccountRef?.name : 
@@ -211,38 +346,94 @@ export default function InvoiceView() {
             const actualQty = item.GroupLineDetail?.Line[0]?.SalesItemLineDetail?.Qty ?? item.SalesItemLineDetail?.Qty;
             const actualPkg = item.GroupLineDetail?.Quantity ?? item.SalesItemLineDetail?.Qty;
             const actualPrice = item.GroupLineDetail?.Line[0]?.SalesItemLineDetail?.UnitPrice ?? item.SalesItemLineDetail?.UnitPrice;
-              return <tr key={index}>
-                <td>{/* Product code */}</td>
-                <td>{actualPkg}</td>
-                <td>{/* Packaging */}</td>
+              return <tr key={index} style={{cursor: 'pointer'}} onClick={() => setInvoiceLine((pv) => [...pv.filter((lineItem) => lineItem.Id !== item.Id)])}>
+                <td>{
+                  getProductCodeFromDesc(itemDescription?.toLocaleLowerCase() ?? "")
+              }</td>
+                <td style={{textAlign: "center"}}>{actualPkg}</td>
+                <td>{getPackagingFromDesc(itemDescription?.toLocaleLowerCase() ?? "")}</td>
                 <td>{itemDescription === 'Discounts given' ? 'Discount' : itemDescription}</td>
-                <td>{/* Lot number */}</td>
+                <td style={{textAlign: "center"}}>{itemDescription?.toLocaleLowerCase().includes("orange") ? "240704" : "240203"}</td>
                 <td>{actualQty}</td>
-                <td>{ item.DetailType === 'DiscountLineDetail' ? 'Percent' : 'Unit' }</td>
-                <td>{/* Weight */}</td>
-                <td>${actualPrice?.toFixed(2)}</td>
-                <td>${item.Amount.toFixed(2)}</td>
+                <td style={{textAlign: "center"}}>{ item.DetailType === 'DiscountLineDetail' ? 'Percent' : 'Unit' }</td>
+                <td style={{textAlign: "center"}}>{getWeightFromLine(itemDescription?.toLocaleLowerCase() ?? "", actualPkg!, actualQty!)}</td>
+                <td style={{textAlign: "right"}}>${Number(actualPrice?.toFixed(2)).toLocaleString()}</td>
+                <td style={{textAlign: "right"}}>${Number(item.Amount.toFixed(2)).toLocaleString()}</td>
               </tr>
           })}
           <tr>
             <th>Total Packages</th>
-            <th>{invoice?.Line
+            <th style={{textAlign: "center"}}>{invoiceLine
             ?.filter(item => ['SalesItemLineDetail','GroupLineDetail','DiscountLineDetail'].includes(item.DetailType)).reduce((acc, item) => {
               const actualQty = item.GroupLineDetail?.Quantity ?? item.SalesItemLineDetail?.Qty;
               return acc + Number(actualQty ?? 0);
               }, 0)}</th>
             <th colSpan={3}></th>
             <th colSpan={2}>Total Weight in KGS: </th>
-            <th></th>
-            <th>Total Invoice</th>
-            <th>${invoice?.Line
-            ?.filter(item => ['SalesItemLineDetail','GroupLineDetail','DiscountLineDetail'].includes(item.DetailType) && item?.SalesItemLineDetail?.ItemRef.value !== "SHIPPING_ITEM_ID" && !item.SalesItemLineDetail?.ItemRef?.name?.includes('Stripe') && !item.GroupLineDetail?.GroupItemRef?.name.includes('Stripe')).reduce((acc, sum) => acc + sum.Amount, 0)}</th>
+            <th style={{textAlign: "center"}}>{getTotalWeight(invoiceLine)}</th>
+            <th style={{textAlign: "center"}}>Total Invoice</th>
+            <th style={{textAlign: "right"}}>${Number(invoiceLine
+            ?.filter(item => ['SalesItemLineDetail','GroupLineDetail','DiscountLineDetail'].includes(item.DetailType) && item?.SalesItemLineDetail?.ItemRef.value !== "SHIPPING_ITEM_ID" && !item.SalesItemLineDetail?.ItemRef?.name?.includes('Stripe') && !item.GroupLineDetail?.GroupItemRef?.name.includes('Stripe')).reduce((acc, sum) => acc + sum.Amount, 0).toFixed(2)).toLocaleString()}</th>
           </tr>
           <tr>
             <th colSpan={10} style={{textAlign: "center"}}>Gripping Device- non medical device</th>
           </tr>
         </tbody>
       </InvoiceTable>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        RETURN TO: ________________________________________________
+      </ExtendedRow>
+      <br/>
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        DOES NOT CONTAIN ANY ANIMAL PRODUCT
+      </ExtendedRow>
+      <br/>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        VALUE FOR CUSTOMS PURPOSES ONLY
+      </ExtendedRow>
+      <br/>
+      <br/>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        I DECLARE ALL THE INFORMATION CONTAINED IN THIS INVOICE TO BE TRUE AND CORRECT
+      </ExtendedRow>
+      <br/>
+      <br/>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        SIGNATURE OF SHIPPER/EXPORTER (Type name and title and sign)
+      </ExtendedRow>
+      <br/>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 70, justifyContent: "flex-start"}}>
+        <b>________________________________________________________________________________________________</b><b style={{textDecoration: "underline"}}>{invoice?.ShipDate}</b>
+      </ExtendedRow>
+      <br/>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        SIGNATURE OF SHIPPER/EXPORTER (Type name and title and sign)
+      </ExtendedRow>
+      <br/>
+      <ExtendedRow style={{alignItems: "center", gap: 70, justifyContent: "flex-start"}}>
+        <b>________________________________________________________________________________________________</b><b style={{textDecoration: "underline"}}>{invoice?.ShipDate}</b>
+      </ExtendedRow>
+      <br/>
+      <br/>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        <b>Transport information</b>
+      </ExtendedRow>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        1. Customs
+      </ExtendedRow>
+      
+      <ExtendedRow style={{alignItems: "center", gap: 4}}>
+        2. Shipping
+      </ExtendedRow>
+      <br/>
     </PrintableContainer>
   );
 }
